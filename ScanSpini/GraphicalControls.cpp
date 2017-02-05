f8b32fc93bb75b7abe91ad91a279712b9cbc4799
Dorian Sharevich <dorian.sharevich@gmail.com>
2017-02-04 19:01:37 -0800

    Add middle GL windows to controller

--- ScanSpini/GraphicalControls.cpp
+++ ScanSpini/GraphicalControls.cpp
@@ -12,6 +12,7 @@
 #include <QtWidgets\qlayout.h>
 #include <QtWidgets\qlabel.h>
 #include "LeftGLWindow.h"
+#include "MiddleGLWindow.h"
 #include "Data3D.h"
 
 
@@ -20,11 +21,15 @@ GraphicalControls::GraphicalControls(Data3D *data3D)
 	this->data3D = data3D;
 	QLayout *graphicsControlsLayout;
 	setLayout(graphicsControlsLayout = new QHBoxLayout);
+	
 	leftGLWindow = new LeftGLWindow(data3D);
-
 	graphicsControlsLayout->addWidget(leftGLWindow);
 	leftGLWindow->setMinimumSize(600, 600);
-	
+
+	middleGLWindow = new MiddleGLWindow(data3D);
+	graphicsControlsLayout->addWidget(middleGLWindow);
+	middleGLWindow->setMinimumSize(600, 600);
+
 	//////////////////////////////////////////////////////////////////////////////
 	QLabel* fileLabel2;
 	graphicsControlsLayout->addWidget(fileLabel2 = new QLabel);
@@ -38,6 +43,7 @@ GraphicalControls::GraphicalControls(Data3D *data3D)
 void GraphicalControls::reInitilizeLeftGLWindow()
 {
 	this->leftGLWindow->callUpdate();
+	this->middleGLWindow->callUpdate();
 }
 
 GraphicalControls::~GraphicalControls()
